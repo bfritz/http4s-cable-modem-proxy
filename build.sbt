27007ccce57d99a5d45bfcb39fbe7a9b3ee57632
Brad Fritz <brad@fewerhassles.com>
2016-12-02 16:53:13 -0500

    stub HTML scraping with scala-scraper
    Had to revert to Scala 2.11.x because scala-scraper is not built
    for 2.12 yet.

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 name := "http4s-proxy-demo"
-scalaVersion := "2.12.0"
+scalaVersion := "2.11.8"
 
 val http4sVersion = "0.15.0a"
 
@@ -7,5 +7,6 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-dsl" % http4sVersion,
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
   "org.http4s" %% "http4s-blaze-client" % http4sVersion,
+  "net.ruippeixotog" %% "scala-scraper" % "1.1.0",
   "org.slf4j" % "slf4j-simple" % "1.7.21"
 )
--- src/main/scala/Proxy.scala
+++ src/main/scala/Proxy.scala
@@ -1,3 +1,7 @@
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract.text
+
 import org.http4s.client.blaze._
 
 object Proxy {
@@ -6,7 +10,14 @@ object Proxy {
     val httpClient = PooledHttp1Client()
     val request = httpClient.expect[String]("http://localhost:8080/index.html")
     val out = request.run
-    print(s"Page: $out")
+    println(s"Page: $out")
+
+    val browser = JsoupBrowser()
+    val doc = browser.parseString(out)
+    val paraText = doc >> text("p")
+
+    println(s"Text: $paraText")
+
     httpClient.shutdownNow()
   }
 }

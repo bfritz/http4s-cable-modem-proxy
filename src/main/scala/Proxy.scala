cc759627162fa9527c41fad7d509f4a92b50f7e8
Brad Fritz <brad@fewerhassles.com>
2016-12-03 10:13:46 -0500

    begin parsing cable modem HTML to extract status

--- src/main/scala/Proxy.scala
+++ src/main/scala/Proxy.scala
@@ -6,17 +6,21 @@ import org.http4s.client.blaze._
 
 object Proxy {
 
+  private val StatusCellSelector = "table:eq(0) tr:eq(7) td:eq(1)"
+  private val UptimeCellSelector = "table:eq(1) tr:eq(2) td:eq(1)"
+
   def main(args: Array[String]) {
     val httpClient = PooledHttp1Client()
-    val request = httpClient.expect[String]("http://localhost:8080/index.html")
-    val out = request.run
-    println(s"Page: $out")
+    val request = httpClient.expect[String]("http://localhost:8080/indexData.htm")
+    val pageHtml = request.run
 
     val browser = JsoupBrowser()
-    val doc = browser.parseString(out)
-    val paraText = doc >> text("p")
+    val doc = browser.parseString(pageHtml)
+    val status: Option[String] = doc >?> text(StatusCellSelector)
+    val uptimeText: Option[String] = doc >?> text(UptimeCellSelector)
 
-    println(s"Text: $paraText")
+    println(s"status: $status")
+    println(s"uptime: $uptimeText")
 
     httpClient.shutdownNow()
   }

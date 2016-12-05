ae2cc9d8667513cdc4a9d94c5ae85b6678a70261
Brad Fritz <brad@fewerhassles.com>
2016-12-04 21:24:50 -0500

    marshal modem status to JSON
    Also factor the HTTP request and parsing out of `main()`.

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
   "org.http4s" %% "http4s-blaze-client" % http4sVersion,
 
+  "io.argonaut" %% "argonaut" % "6.2-RC2",
   "net.ruippeixotog" %% "scala-scraper" % "1.1.0",
   "org.log4s" %% "log4s" % "1.3.3",
   "org.slf4j" % "slf4j-simple" % "1.7.21"
--- src/main/scala/Proxy.scala
+++ src/main/scala/Proxy.scala
@@ -1,17 +1,38 @@
+import argonaut._, Argonaut._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract.text
 
+import org.http4s.client.Client
 import org.http4s.client.blaze._
+
+import scalaz._
+
+case class Status(currentStatus: Option[String], uptime: Option[String])
+
+object Status {
+  implicit def StatusCodecJson: CodecJson[Status] =
+    casecodec2(Status.apply, Status.unapply)("status", "uptime")
+}
 
 object Proxy {
 
   private val StatusCellSelector = "table:eq(0) tr:eq(7) td:eq(1)"
   private val UptimeCellSelector = "table:eq(1) tr:eq(2) td:eq(1)"
+  private val StatusPage = "http://localhost:8080/indexData.htm"
 
   def main(args: Array[String]) {
     val httpClient = PooledHttp1Client()
-    val request = httpClient.expect[String]("http://localhost:8080/indexData.htm")
+
+    for {
+      status <- getStatus(httpClient)
+    } println(s"status: ${status.asJson.spaces2}")
+
+    httpClient.shutdownNow()
+  }
+
+  def getStatus(client: Client): \/[String, Status] = {
+    val request = client.expect[String](StatusPage)
     val pageHtml = request.run
 
     val browser = JsoupBrowser()
@@ -19,9 +40,6 @@ object Proxy {
     val status: Option[String] = doc >?> text(StatusCellSelector)
     val uptimeText: Option[String] = doc >?> text(UptimeCellSelector)
 
-    println(s"status: $status")
-    println(s"uptime: $uptimeText")
-
-    httpClient.shutdownNow()
+    \/-(Status(status, uptimeText))
   }
 }
